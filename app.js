0c4b4ef0173de31ab4aa738dbf3bb2da43284956
Shreyass48 <sgkulkarni48@gmail.com>
2026-03-17 00:54:20 +0530

    CLI based chatbot code added with webSearch tool

--- app.js
+++ app.js
@@ -1,67 +1,97 @@
 import "dotenv/config";
+import readline from "node:readline/promises";
 import Groq from "groq-sdk";
 import { webSearch } from "./tools.js";
 
 const groq = new Groq({ apiKey: process.env.GROQ_LLM_KEY });
 
 async function main() {
-  const completion = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: [
-      {
-        role: "system",
-        content: `You are a smart assistant for answering questions about the world. 
-        You have access to a following tools 
-        1. webSearch({q}:{query: string}): Search the latest information about the world on the web.
-        `,
-      },
-      {
-        role: "user",
-        content: `When was i phone 17 launched?`,
-      },
-    ],
-    // added tools to the model
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description:
-            "Search the latest information about the world on the web",
-          parameters: {
-            // JSON Schema object
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform search on.",
-              },
-            },
-            required: ["query"],
-          },
-        },
-      },
-    ],
-    tool_choice: "auto",
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  const tool_calls = completion.choices[0].message.tool_calls;
+  const messages = [
+    {
+      role: "system",
+      content: `You are a smart assistant for answering questions about the world. 
+        You have access to a following tools 
+        1. webSearch({q}:{query: string}): Search the latest information about the world on the web.
+        
+        current Date and Time: ${new Date().toUTCString()}`,
+    },
+  ];
 
-  if (!tool_calls) {
-    console.log(completion.choices[0].message.content);
-    return;
-  }
+  while (true) {
+    const question = await rl.question("You: ");
 
-  for (const tool of tool_calls) {
-    const functionName = tool.function.name;
-    const functionArgs = tool.function.arguments;
+    if (question.toLowerCase() === "exit") {
+      console.log("Exiting...");
+      break;
+    }
 
-    if (functionName === "webSearch") {
-      const result = await webSearch(JSON.parse(functionArgs));
-      //   console.log(`Result from ${functionName}: ${result}`);
+    messages.push({ role: "user", content: question });
+
+    // LLM react LOOP
+    while (true) {
+      const completion = await groq.chat.completions.create({
+        model: "llama-3.3-70b-versatile",
+        temperature: 0,
+        messages: messages,
+        // added tools to the model
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "webSearch",
+              description:
+                "Search the latest information about the world on the web",
+              parameters: {
+                // JSON Schema object
+                type: "object",
+                properties: {
+                  query: {
+                    type: "string",
+                    description: "The search query to perform search on.",
+                  },
+                },
+                required: ["query"],
+              },
+            },
+          },
+        ],
+        tool_choice: "auto",
+      });
+
+      // pushing the assistant response to messages so that LLM can have the context of previous tool calls and their responses
+      messages.push(completion.choices[0].message);
+
+      const tool_calls = completion.choices[0].message.tool_calls;
+
+      if (!tool_calls) {
+        console.log(`Assistant: ${completion.choices[0].message.content}`);
+        break;
+      }
+
+      for (const tool of tool_calls) {
+        const functionName = tool.function.name;
+        const functionArgs = tool.function.arguments;
+
+        if (functionName === "webSearch") {
+          const toolResult = await webSearch(JSON.parse(functionArgs));
+
+          // for LLM to summerize the tool result and answer the question asked by user
+          messages.push({
+            tool_call_id: tool.id,
+            role: "tool",
+            name: functionName,
+            content: toolResult,
+          });
+        }
+      }
     }
   }
+  rl.close();
 }
 
 main();
--- tools.js
+++ tools.js
@@ -4,7 +4,12 @@ import { tavily } from "@tavily/core";
 const tvly = tavily({ apiKey: process.env.TVLY_API_KEY });
 
 export async function webSearch({ query }) {
+  console.log(`Searching on web...`);
   const response = await tvly.search(query);
-  console.log(response);
-  return response;
+  const finalresult = response.results
+    .map((result) => {
+      return result.content;
+    })
+    .join("\n\n");
+  return finalresult;
 }
